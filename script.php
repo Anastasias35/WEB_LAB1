a9afb5ed393eca6949457323902700df80f32620
Anastasias35 <asypacheva35@gmail.com>
2021-09-19 23:42:51 +0300

    add request from server and response to server

--- script.php
+++ script.php
@@ -1,8 +1,5 @@
 <?php
-$coordx=$_GET["Coordinates_X"];
-$coordy=$_GET["Coordinates_Y"];//пока не добавлено
-$radius=$_GET["Radius"];
-
+session_start();
 function validateX($coordx){
     if (!isset($coordx)) return false;
     else if (!is_numeric($coordx)) return false;
@@ -22,12 +19,29 @@ function validate($coordx,$coordy,$radius){
     return validateX($coordx)&&validateY($coordy)&&validateR($radius);
 }
 
-function checkArea($coordx,$coordy,$radius){ // проверка попадания в область
-    return ($coordx>=0 && $coordx<=$radius && $coordy<=0 && $coordy>=-$radius) || ($coordx<=0 && $coordy>=0 && $coordx*$coordx + $coordy*$coordy<=$radius*$radius) || ($coordx<=0 && $coordy<=0 &&  -2*$coordx - $radius<=$coordy);
+function checkArea($x,$y,$r){ // проверка попадания в область
+    return ($x>=0 && $x<=$r && $y<=0 && $y>=-$r) || ($x<=0 && $y>=0 && $x*$x + $y*$y<=$r*$r) || ($x<=0 && $y<=0 &&  -2*$x - $r<=$y);
 }
 
+$coordx=$_GET['Coordinates_X'];
+$coordy=$_GET['Coordinates_Y'];
+$radius=$_GET['Radius'];
+$timezone=$_GET['timezone'];
+$curtime=date("H:i:s",time()-$timezone*60);
 
 
+$time=round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],7);
 
+$validation=validate($coordx,$coordy,$radius);
+$check=checkArea($coordx,$coordy,$radius)?'true':'false';
 
-
+$itog='{' .
+    "\"validate\": $validation," .
+    "\"xval\":\"$coordx\"," .
+    "\"yval\":\"$coordy\"," .
+    "\"rval\":\"$radius\"," .
+    "\"curtime\":\"$curtime\"," .
+    "\"exectime\":\"$time\"," .
+    "\"checkarea\":\"$check\"".
+    "}";
+echo $itog;
