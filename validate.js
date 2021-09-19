abcf8301c527a90b770f50f0e43b3f339e6383bc
Anastasias35 <asypacheva35@gmail.com>
2021-09-19 23:39:53 +0300

    add button and fixed validation

--- validate.js
+++ validate.js
@@ -1,180 +1,80 @@
-var count_y=0;
-var check_y1=false;
-var check_y2=false;
-var check_y3=false;
-var check_y4=false;
-var check_y5=false;
-var check_y6=false;
-var check_y7=false;
-var check_y8=false;
-var check_y9=false;
+$(document).ready(function(){
+    $('#button-y :button').click(function (){
+        $('#button-y :button').removeClass("active");
+        $(this).addClass("active");
 
-$('#y1').click(function() {
-    if (!check_y1){
-        $('#y1').css('background','lavender');
-        count_y ++;
-        check_y1=true;
-    }else{
-        $('#y1').css('background','white');
-        count_y --;
-        check_y1=false;
-    }
-});
-
-$('#y2').click(function() {
-    if (!check_y2){
-        $('#y2').css('background','lavender');
-        count_y ++;
-        check_y2=true;
-    }else{
-        $('#y2').css('background','white');
-        count_y --;
-        check_y2=false;
-    }
-
-});
-
-$('#y3').click(function() {
-    if (!check_y3){
-        $('#y3').css('background','lavender');
-        count_y ++;
-        check_y3=true;
-    }else{
-        $('#y3').css('background','white');
-        count_y --;
-        check_y3=false;
-    }
-});
-
-$('#y4').click(function() {
-    if (!check_y4){
-        $('#y4').css('background','lavender');
-        count_y ++;
-        check_y4=true;
-    }else{
-        $('#y4').css('background','white');
-        count_y --;
-        check_y4=false;
-    }
-});
-
-$('#y5').click(function() {
-    if (!check_y5){
-        $('#y5').css('background','lavender');
-        count_y ++;
-        check_y5=true;
-    }else{
-        $('#y5').css('background','white');
-        count_y --;
-        check_y5=false;
-    }
-});
-
-$('#y6').click(function() {
-    if (!check_y6){
-        $('#y6').css('background','lavender');
-        count_y ++;
-        check_y6=true;
-    }else{
-        $('#y6').css('background','white');
-        count_y --;
-        check_y6=false;
-    }
-});
-$('#y7').click(function() {
-    if (!check_y7){
-        $('#y7').css('background','lavender');
-        count_y ++;
-        check_y7=true;
-    }else{
-        $('#y7').css('background','white');
-        count_y --;
-        check_y7=false;
-    }
-});
-$('#y8').click(function() {
-    if (!check_y8){
-        $('#y1').css('background','lavender');
-        count_y ++;
-        check_y8=true;
-    }else{
-        $('#y8').css('background','white');
-        count_y --;
-        check_y8=false;
-    }
-});
-
-$('#y9').click(function() {
-    if (!check_y9){
-        $('#y9').css('background','lavender');
-        count_y ++;
-        check_y9=true;
-    }else{
-        $('#y9').css('background','white');
-        count_y --;
-        check_y9=false;
-    }
+    });
 });
 
 function validateX(){
+
     let x=document.forms["form"]["Coordinates_X"].value;
+    x=x.replaceAll(",",".");
     if (x.trim()===""){
-       alert("введите значение для координаты x");
-       return false;
+        alert("введите значение для координаты x");
+        return false;
     }
     else if (isNaN(x)){
         alert("координата x должна быть числом");
         return false;
     }
-    else if (x <= -3 || x>= 3) {
+    else if (Math.floor(x) <= -3 || Math.floor(x)>= 3) {
         alert("координата x должна быть в диапозоне:(-3;3)");
         return false;
     }
     return true;
 }
 
-
 function validateY(){
-    if (count_y==0){
-        alert("Выберите значение координаты y");
+    active=false;
+    $('.active').each(function (){
+        active=true;
+    })
+    if(!active){
+        alert("выберите одно из значение координаты y");
         return false;
     }
-    else if (count_y!=1){
-        alert("Выберите только одно значение координаты y");
-        return false;
+    else{
+        return true;
     }
-    return true;
 }
 
 function validate(){
-    validateX();
-    validateY();
+    return validateX()&&validateY();
 }
 
 $('.reset-button').click(function (){
     $('.form')[0].reset();
-    check_y1=check_y2=check_y3=check_y4=check_y5=check_y6=check_y7=check_y8=check_y9=false;
-    $('#y1').css('background','white');
-    $('#y2').css('background','white');
-    $('#y3').css('background','white');
-    $('#y4').css('background','white');
-    $('#y5').css('background','white');
-    $('#y6').css('background','white');
-    $('#y7').css('background','white');
-    $('#y8').css('background','white');
-    $('#y9').css('background','white');
+    $('#button-y :button').removeClass("active");
+})
+
+$('.form').on('submit',function (event){
+    event.preventDefault();
+    if(!validate()) return;
+    else{
+        let data = $(this).serialize()+'&Coordinates_Y=' +document.getElementsByClassName("active")[0].value  + '&timezone=' + new Date().getTimezoneOffset();
+        var request = new XMLHttpRequest();
+        request.open('GET', 'https://se.ifmo.ru/~s311724/lab1/script.php'+'?'+data,true);
+        request.send();
+        request.onload = function () {
+            if (request.status != 200) {
+                alert(`Ошибка ${request.status}: ${request.statusText}`);
+            } else {
+                console.log(request.responseText);
+                let result = JSON.parse(request.responseText);
+                if (result.validate) {
+                    row = '<tr>';
+                    row += '<td>' + result.xval + '</td>';
+                    row += '<td>' + result.yval + '</td>';
+                    row += '<td>' + result.rval + '</td>';
+                    row += '<td>' + result.checkarea + '</td>';
+                    row += '<td colspan="2">' + result.curtime + '</td>';
+                    row += '<td colspan="2">' + result.exectime + '</td>';
+                    $('.table-result').append(row);
+                }
+            }
+        }
+        $('.form')[0].reset();
+        $('#button-y :button').removeClass("active");
+    }
 });
-
-
-$(' .submit_button').on(function (){
-    if (!validateX()&&validateY()) return;
-
-});
-
-
-
-
-
-
-
-document.getElementsByClassName("submit_button")[0].addEventListener("click", validate);
